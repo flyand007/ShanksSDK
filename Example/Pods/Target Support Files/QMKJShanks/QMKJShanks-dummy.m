49e06e5407bbe5a4ef36f519063e8882b6dbc73c
lijian <lijian@quanjing.com>
2025-08-21 18:28:56 +0800

    dd

---
+++ Example/Pods/Target Support Files/QMKJShanks/QMKJShanks-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_QMKJShanks : NSObject
+@end
+@implementation PodsDummy_QMKJShanks
+@end
---
+++ Example/QMKJShanks/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  QMKJShanks
+//
+//  Created by flyand007 on 08/21/2025.
+//  Copyright (c) 2025 flyand007. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
